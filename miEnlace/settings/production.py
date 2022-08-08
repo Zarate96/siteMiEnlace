989e2f457bd6c446f235e23c40c6b3d269c91343
Your Name <you@example.com>
2022-08-08 18:09:10 -0500

    set up fronted

--- miEnlace/settings/production.py
+++ miEnlace/settings/production.py
@@ -5,7 +5,7 @@ from miEnlace.settings.base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mienlacemexico.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Database
