8669c4d55bc33f532ef5f2b3bb83dd06019c91e0
Your Name <you@example.com>
2022-08-08 15:59:44 -0500

    set up fronted

--- miEnlace/settings/production.py
+++ miEnlace/settings/production.py
@@ -1,7 +1,7 @@
 import os
 import dj_database_url
 from decouple import config
-from .base import *
+from miEnlace.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
